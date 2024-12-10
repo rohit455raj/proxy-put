3e2826ef12abaf645eaee9428a3a868b57d3842b
rohit455raj <mr.rajarohit06540@gmail.com>
2024-12-11 02:36:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import requests
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/proxy', methods=['POST'])
 def proxy_post():
